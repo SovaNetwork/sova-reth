935ebc364535b0f02cc83f84ab75f0f3c9defca4
Debjit <github@debjit.dev>
2025-07-22 21:42:50 +0530

    fix: restore deposit contract references in dev and mainnet specifications

--- crates/chainspec/src/mainnet.rs
+++ crates/chainspec/src/mainnet.rs
@@ -3,20 +3,16 @@ use std::{
     sync::{Arc, LazyLock},
 };
 
-use alloy_genesis::{Genesis, GenesisAccount};
-use alloy_primitives::{address, b256, Address, Bytes, U256};
+use alloy_genesis::Genesis;
+use alloy_primitives::{address, b256, Bytes, U256};
 
-use reth_chainspec::{Chain, DepositContract};
+use reth_chainspec::Chain;
 use reth_optimism_chainspec::{OpChainSpec, OpChainSpecBuilder};
 
-use crate::constants::{
-    deposit_contract_storage, sova_forks, DEPOSIT_CONTRACT_ADDRESS, DEPOSIT_CONTRACT_CODE,
-};
+use crate::constants::sova_forks;
 
 /// Sova main chain specification.
 pub static SOVA: LazyLock<Arc<OpChainSpec>> = LazyLock::new(|| {
-    let deposit_contract_storage = deposit_contract_storage();
-
     let genesis = Genesis::default()
         .with_nonce(0x01d83d)
         .with_timestamp(0x673e4f9b)
@@ -26,29 +22,13 @@ pub static SOVA: LazyLock<Arc<OpChainSpec>> = LazyLock::new(|| {
         .with_mix_hash(b256!(
             "0000000000000000000000000000000000000000000000000000000000000000"
         ))
-        .with_coinbase(address!("0000000000000000000000000000000000000000"))
-        .extend_accounts(vec![
-            // PoS deposit contract
-            (
-                Address::from_str(DEPOSIT_CONTRACT_ADDRESS).unwrap(),
-                GenesisAccount::default()
-                    .with_code(Some(Bytes::from_str(DEPOSIT_CONTRACT_CODE).unwrap()))
-                    .with_storage(Some(deposit_contract_storage))
-                    .with_balance(U256::from(0)),
-            ),
-        ]);
+        .with_coinbase(address!("0000000000000000000000000000000000000000"));
 
-    let mut spec: OpChainSpec = OpChainSpecBuilder::default()
+    let spec: OpChainSpec = OpChainSpecBuilder::default()
         .chain(Chain::from_id(120893))
         .genesis(genesis)
         .with_forks(sova_forks())
         .build();
-
-    spec.inner.deposit_contract = Some(DepositContract::new(
-        Address::from_str(DEPOSIT_CONTRACT_ADDRESS).unwrap(),
-        0,
-        b256!("649bbc62d0e31342afea4e5cd82d4049e7e1ee912fc0889aa790803be39038c5"),
-    ));
 
     spec.into()
 });
