a4675946205fbdaef27ee6a308193d8a0cab77ae
PowVT <epowlowsky@gmail.com>
2025-10-29 12:13:02 -0400

    revert xpub (#150)

--- chainspec/src/mainnet.rs
+++ chainspec/src/mainnet.rs
@@ -8,7 +8,7 @@ use crate::SovaChainSpec;
 use super::constants::sova_forks;
 
 /// Sova mainnet derivation xpub
-pub const SOVA_MAINNET_DERIVATION_XPUB: &str = "xpub661MyMwAqRbcGdAvHLio9QdLWhMTbnfa27fZSD5quMusfEwxeyrXrbMrkvoPzQ2bcMAeMGbwHDueBpgHRjuHLfR2hFot14QgKqaWrL8PSAj";
+pub const SOVA_MAINNET_DERIVATION_XPUB: &str = "xpub6BebdYenb6pkUyW4zCn2QT7PMN3eN8mpEEvTkrrWpQFrzoaKTKhaQQS8VSyS7VGCZTfNX3fKQ6WqgQM459yb3gRojzZmwnLdUN2PcLkg9Q4";
 
 pub static SOVA: LazyLock<Arc<SovaChainSpec>> = LazyLock::new(|| {
     let genesis = serde_json::from_str(include_str!("res/genesis/sova.json"))
